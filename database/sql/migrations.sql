556e26fb30db3943f665dbc730e55e1ea11d88f8
Paulo Panissa Filho <p.panissa@gmail.com>
2018-10-02 11:48:41 -0400

    sql files

---
+++ database/sql/migrations.sql
@@ -0,0 +1,2 @@
+INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (1, '2018_10_01_145023_create_table_cadastro_de_pessoas', 1);
+INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (2, '2018_10_01_145053_create_table_cadastro_de_dependentes', 1);
